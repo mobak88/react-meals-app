c4eb2c5d01c069fe2c5d3e843ce132a274579c2c
mobak88 <mostafa0401@ymail.com>
2022-11-28 16:47:23 +0100

    Condionilly rendering liked meals page

--- src/pages/likedMeals/LikedMeals.jsx
+++ src/pages/likedMeals/LikedMeals.jsx
@@ -3,27 +3,37 @@ import IsLoggedInContext from "../../contexts/isLoggedInContext";
 import MealCard from "../../components/cards/MealCard";
 import MealCardContainer from "../../components/ui/mealCardContainer/MealCardContainer";
 import MealsWrapper from "../../components/ui/mealsWrapper/MealsWrapper";
+import CenterContainer from "../../components/ui/centerContainer/CenterContainer";
 
 const LikedMeals = () => {
   const { isLoggedIn, likedRecipes } = useContext(IsLoggedInContext);
   return (
-    <MealsWrapper>
-      <h1>Your liked meals</h1>
-      <MealCardContainer meals={likedRecipes}>
-        {likedRecipes.length > 0 &&
-          likedRecipes.map((recipe) => {
-            return (
-              <MealCard
-                key={recipe.idMeal}
-                mealId={recipe.idMeal}
-                mealName={recipe.strMeal}
-                mealImg={recipe.strMealThumb}
-                meals={likedRecipes}
-              />
-            );
-          })}
-      </MealCardContainer>
-    </MealsWrapper>
+    <>
+      {isLoggedIn && (
+        <MealsWrapper>
+          <h1>Your liked meals</h1>
+          <MealCardContainer meals={likedRecipes}>
+            {likedRecipes.length > 0 &&
+              likedRecipes.map((recipe) => {
+                return (
+                  <MealCard
+                    key={recipe.idMeal}
+                    mealId={recipe.idMeal}
+                    mealName={recipe.strMeal}
+                    mealImg={recipe.strMealThumb}
+                    meals={likedRecipes}
+                  />
+                );
+              })}
+          </MealCardContainer>
+        </MealsWrapper>
+      )}
+      {!isLoggedIn && (
+        <CenterContainer>
+          <h1>You are not logged in</h1>
+        </CenterContainer>
+      )}
+    </>
   );
 };
 
