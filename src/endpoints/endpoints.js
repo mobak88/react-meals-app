5fd8d96c17531e6b4a2412b43643954578aa4dee
mobak88 <mostafa0401@ymail.com>
2022-11-30 12:46:51 +0100

    Adding endpoint for areas

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -2,8 +2,12 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const API_ENDPOINTS = {
     categories: `${BASE_URL}/list.php?c=list`,
+    areas: `${BASE_URL}/list.php?a=list`,
     filterCategory: (category) => {
         return `${BASE_URL}/filter.php?c=${category}`;
+    },
+    filterArea: (area) => {
+        return `${BASE_URL}/filter.php?a=${area}`;
     },
     fullMeal: (mealId) => {
         return `${BASE_URL}/lookup.php?i=${mealId}`;
