2573400fe84683a084143d842822be307d2e2b21
mobak88 <mostafa0401@ymail.com>
2022-11-30 12:48:05 +0100

    Creating meal areas page

---
+++ src/pages/mealsByArea/MealsByArea.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import MealLinks from "../../components/mealLinks/MealLinks";
+import MealCategorySection from "../../components/sections/MealCategorySection";
+import API_ENDPOINTS from "../../endpoints/endpoints";
+import useFetch from "../../hooks/useFetch";
+
+const MealsByArea = () => {
+  const [mealAreas, setMealAreas] = useState([]);
+
+  const { loading, err, data } = useFetch(API_ENDPOINTS.areas);
+
+  useEffect(() => {
+    if (data?.meals !== null && data?.meals !== undefined) {
+      setMealAreas([...data.meals]);
+    }
+  }, [data]);
+
+  if (err) return `Error: ${err}`;
+
+  if (loading) return `...Loading`;
+
+  return (
+    <>
+      <MealLinks
+        mealArr={mealAreas}
+        mealKey={"strArea"}
+        categoryType={"area"}
+      />
+      {mealAreas &&
+        mealAreas.map((area) => {
+          return (
+            <MealCategorySection
+              key={area.strArea}
+              mealCategory={area.strArea}
+              filterType="filterArea"
+            />
+          );
+        })}
+    </>
+  );
+};
+
+export default MealsByArea;
