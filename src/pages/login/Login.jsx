d22bce2eb962ae280d51eeb8392b7df01f79249e
mobak88 <mostafa0401@ymail.com>
2022-12-01 10:22:11 +0100

    Adding feedback to login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,10 @@
-import { useContext, useRef, useEffect } from "react";
+import { useContext, useRef, useEffect, useState } from "react";
 import LoginForm from "../../components/ui/forms/loginForm/LoginForm";
 import CenterContainer from "../../components/ui/centerContainer/CenterContainer";
 import IsLoggedInContext from "../../contexts/isLoggedInContext";
 
 const Login = () => {
+  const [errMsg, setErrMsg] = useState("");
   const { isLoggedIn, setIsLoggedIn } = useContext(IsLoggedInContext);
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
@@ -19,10 +20,19 @@ const Login = () => {
     const userPassword = "1234";
 
     e.preventDefault();
+
+    if (
+      emailInputRef.current.value !== userEmail ||
+      passwordInputRef.current.value !== userPassword
+    ) {
+      setErrMsg("Wrong email or password");
+    }
+
     if (
       emailInputRef.current.value === userEmail &&
       passwordInputRef.current.value === userPassword
     ) {
+      setErrMsg("");
       setIsLoggedIn(true);
       emailInputRef.current.value = "";
       passwordInputRef.current.value = "";
@@ -32,11 +42,14 @@ const Login = () => {
   return (
     <CenterContainer>
       {!isLoggedIn && (
-        <LoginForm
-          emailInputRef={emailInputRef}
-          passwordInputRef={passwordInputRef}
-          onSubmitForm={handleSubmit}
-        />
+        <>
+          <LoginForm
+            emailInputRef={emailInputRef}
+            passwordInputRef={passwordInputRef}
+            onSubmitForm={handleSubmit}
+          />
+          {errMsg.length > 0 && <p>{errMsg}</p>}
+        </>
       )}
       {isLoggedIn && <h1>You are logged in</h1>}
     </CenterContainer>
